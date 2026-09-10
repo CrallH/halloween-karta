4d0ab6cf61f714169115745c1bffa049d9a4d98e
CrallH <32394267+CrallH@users.noreply.github.com>
2026-09-10 17:55:48 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Inställningar för Halloween-kartan
 window.HALLOWEEN = {
   // Klistra in webbapp-länken från Apps Script (Distribuera → Webbapp → URL som slutar på /exec)
-  API_URL: 'https://script.google.com/macros/s/DIN_SCRIPT_ID/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbz6FpHDJx9LfzfzXr0qihvIP_X7GPN5L14TUXE1jL56bEYB_YJlNGgrZefcMJ17vVcw/exec',
   TOWN: 'Kungsbacka',
   CENTER: { lat: 57.487, lng: 12.076, zoom: 13 }
 };
